893eebb002a7af56e15f6aaa93be6e06766d1e7b
artur.osipenko <artur.osipenko@hotmail.com>
2020-03-11 22:42:01 +0200

    fix: image input

--- resources/views/components/inputs/file.blade.php
+++ resources/views/components/inputs/file.blade.php
@@ -1,4 +1,6 @@
-<img src="{{ $value ?? '' }}" alt="" class="custom-file-image" id="{{ $id ?? 'image' }}">
+<div class="text-center my-2">
+    <img src="{{ $value ?? '' }}" alt="" class="custom-file-image" id="{{ $id ?? 'image' }}">
+</div>
 <div class="custom-file">
     <input type="file" name="{{ $name }}" class="custom-file-input" data-image-id="#{{ $id ?? 'image' }}">
     <label class="custom-file-label">
