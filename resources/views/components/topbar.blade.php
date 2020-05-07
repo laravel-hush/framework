3f997d01b30a473d70d73fc8a2e032f00248cd72
scary-layer <artur.osipenko@hotmail.com>
2020-05-07 16:47:24 +0300

    fix: topbar design

--- resources/views/components/topbar.blade.php
+++ resources/views/components/topbar.blade.php
@@ -1,7 +1,12 @@
 <nav class="horizontal-nav row no-gutters align-items-center justify-content-between with-margin" id="topbar">
-    <a href="/" class="logo">Hush</a>
-    <div class="navigation">
-        <div class="user row no-gutters align-items-center rounded-pill">
+    <a href="/" class="logo">
+        <img src="{{ asset('vendor/hush/images/long-logo.png') }}" alt="">
+    </a>
+    <div class="navigation row no-gutters">
+        <div class="navigation-item col notifications">
+            <i class="material-icons">notifications</i>
+        </div>
+        <div class="user row no-gutters align-items-center">
             <div class="name">{{ auth()->user()->name }}</div>
             <div class="image">
                 <img src="/vendor/hush/images/user-placeholder.jpg" class="rounded-circle">
