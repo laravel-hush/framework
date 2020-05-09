d9b6215967d188c12d5dd2eac3a7305347ba670e
scary-layer <artur.osipenko@hotmail.com>
2020-05-09 14:59:27 +0300

    feat: notifications dropdown design

--- resources/views/components/topbar.blade.php
+++ resources/views/components/topbar.blade.php
@@ -3,8 +3,30 @@
         <img src="{{ asset('vendor/hush/images/long-logo.png') }}" alt="">
     </a>
     <div class="navigation row no-gutters">
-        <div class="navigation-item col notifications">
+        <div class="navigation-item col notifications" id="dropdownNotifications" data-toggle="dropdown"
+            aria-haspopup="true" aria-expanded="false">
             <i class="material-icons">notifications</i>
+        </div>
+        <div class="dropdown-menu notifications-dropdown" aria-labelledby="dropdownNotifications">
+            <div class="dropdown-item heading">Notifications</div>
+            <a href="#" class="dropdown-item notification">
+                <div class="icon-col">
+                    <i class="material-icons">report</i>
+                </div>
+                <div class="col">
+                    <p class="message">New report has been received</p>
+                    <p class="time">23 hrs ago</p>
+                </div>
+            </a>
+            <a href="#" class="dropdown-item notification">
+                <div class="icon-col">
+                    <i class="material-icons">notifications</i>
+                </div>
+                <div class="col">
+                    <p class="message">New report has been received</p>
+                    <p class="time">23 hrs ago</p>
+                </div>
+            </a>
         </div>
         <div class="user row no-gutters align-items-center">
             <div class="name">{{ auth()->user()->name }}</div>
