b17fcd8a84d2c9a652e3fb828855a39f10030de9
scary-layer <artur.osipenko@hotmail.com>
2020-06-02 21:20:33 +0300

    feat: slugify for multilingual fields

--- resources/views/components/inputs/text-multilingual.blade.php
+++ resources/views/components/inputs/text-multilingual.blade.php
@@ -2,8 +2,12 @@
     <div class="col">
         @foreach ($langs as $i => $lang)
         {!! Form::text($input['name'] . "[$lang->code]", $values[$lang->code] ?? '', [
-            'class' => 'form-control multilingual-field ' . ($i != 0 ? 'd-none' : '') . ' ' . ($input['class'] ?? ''),
-            'placeholder' => __('hush::admin.' . ($input['placeholder'] ?? $input['label'] ?? ''))
+            'class' => 'form-control multilingual-field '
+                . ($i != 0 ? 'd-none' : '') . ' '
+                . (isset($input['slugify']) && !$model->id ? 'sluggable' : '') . ' '
+                . ($input['class'] ?? ''),
+            'placeholder' => __('hush::admin.' . ($input['placeholder'] ?? $input['label'] ?? '')),
+            'data-slugify-target' => $i == 0 ? ($input['slugify'] ?? null) : null,
         ]) !!}
         @endforeach
     </div>
