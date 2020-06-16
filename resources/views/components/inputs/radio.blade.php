0c9f04aab66d97b31c0630360e6c2c700abbdcea
scary-layer <artur.osipenko@hotmail.com>
2020-06-16 16:11:24 +0300

    feat: input type radio

---
+++ resources/views/components/inputs/radio.blade.php
@@ -0,0 +1,8 @@
+<label class="radio">
+    <input type="radio" name="{{ $name }}" class="form-control d-none" value="{{ $value ?? '1' }}">
+
+    <div class="replacer">
+        <i class="material-icons">done</i>
+    </div>
+    <span class="text">{{ $label ?? '' }}</span>
+</label>
