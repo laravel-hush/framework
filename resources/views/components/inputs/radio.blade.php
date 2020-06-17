5794e0bdd5eaf4b38c451edb56732babe72e0404
scary-layer <artur.osipenko@hotmail.com>
2020-06-17 11:09:18 +0300

    fix: radio is_checked property

--- resources/views/components/inputs/radio.blade.php
+++ resources/views/components/inputs/radio.blade.php
@@ -1,5 +1,6 @@
 <label class="radio">
-    <input type="radio" name="{{ $name }}" class="form-control d-none" value="{{ $value ?? '1' }}">
+    <input type="radio" name="{{ $name }}" class="form-control d-none"
+        value="{{ $value ?? '1' }}" {{ isset($is_checked) && $is_checked ? 'checked' : '' }}>
 
     <div class="replacer">
         <i class="material-icons">done</i>
