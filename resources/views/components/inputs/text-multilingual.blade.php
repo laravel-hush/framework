87b56bc02c7a7f05f01294015131068435cbcae9
scary-layer <artur.osipenko@hotmail.com>
2020-06-18 10:47:04 +0300

    fix: multilingual fields slugification

--- resources/views/components/inputs/text-multilingual.blade.php
+++ resources/views/components/inputs/text-multilingual.blade.php
@@ -2,15 +2,17 @@
 
 <div class="multilingual-input row no-gutters align-items-center">
     <div class="col">
+        @php($mark = true)
         @foreach ($langs as $i => $lang)
         {!! Form::text($input['name'] . "[$lang->code]", $values[$lang->code] ?? '', [
             'class' => 'form-control multilingual-field '
                 . ($i != 0 ? 'd-none' : '') . ' '
-                . (isset($input['slugify']) && !$model->id ? 'sluggable' : '') . ' '
+                . (isset($input['slugify']) && $mark && !$model->id ? 'sluggable' : '') . ' '
                 . ($input['class'] ?? ''),
             'placeholder' => __('hush::admin.' . ($input['placeholder'] ?? $input['label'] ?? '')),
-            'data-slugify-target' => $i == 0 ? ($input['slugify'] ?? null) : null,
+            'data-slugify-target' => $mark ? ($input['slugify'] ?? null) : null,
         ]) !!}
+        @php($mark = false)
         @endforeach
     </div>
     <select class="col-2 multilingual-selector">
@@ -23,17 +25,19 @@
 @else
 
 <div class="row">
+    @php($mark = true)
     @foreach ($langs as $i => $lang)
     <div class="form-group {{ $input['field_width'] ?? "col-12" }}">
         {!! Form::label($input['name'] . "[$lang->code]", __('hush::admin.' . $input['label']) . " ($lang->name)") !!}
         {!! Form::text($input['name'] . "[$lang->code]", $values[$lang->code] ?? '', [
             'class' => 'form-control multilingual-field '
-                . (isset($input['slugify']) && !$model->id ? 'sluggable' : '') . ' '
+                . (isset($input['slugify']) && $mark && !$model->id ? 'sluggable' : '') . ' '
                 . ($input['class'] ?? ''),
                 'placeholder' => __('hush::admin.' . ($input['placeholder'] ?? $input['label'] ?? '')),
-                'data-slugify-target' => $i == 0 ? ($input['slugify'] ?? null) : null,
+                'data-slugify-target' => $mark ? ($input['slugify'] ?? null) : null,
         ]) !!}
     </div>
+    @php ($mark = false)
     @endforeach
 </div>
 
