b56979e976f2add4a441018e093075c9c8e03eed
scary-layer <artur.osipenko@hotmail.com>
2020-08-03 16:09:50 +0300

    feat: attributes for checkbox and radio components + ability to set different backgrounds

--- resources/views/components/inputs/checkbox.blade.php
+++ resources/views/components/inputs/checkbox.blade.php
@@ -1,6 +1,8 @@
-<label class="checkbox" {{ $attributes ?? '' }}>
-    <input type="checkbox" name="{{ $name }}" class="form-control d-none"
-        value="{{ $value }}" {{ $is_checked ? 'checked' : '' }}>
+<label
+    {{ $attributes->filter(fn ($value, $key) => !in_array($key, ['readonly', 'disabled']))->merge(['class' => 'checkbox']) }}>
+    <input type="checkbox" name="{{ $name }}" class="form-control d-none" value="{{ $value }}"
+        {{ $is_checked ? 'checked' : '' }}
+        {{ $attributes->filter(fn ($value, $key) => in_array($key, ['readonly', 'disabled'])) }}>
 
     <div class="replacer">
         <i class="material-icons">done</i>
--- resources/views/components/inputs/radio.blade.php
+++ resources/views/components/inputs/radio.blade.php
@@ -1,6 +1,8 @@
-<label class="radio">
-    <input type="radio" name="{{ $name }}" class="form-control d-none"
-        value="{{ $value }}" {{ $is_checked ? 'checked' : '' }}>
+<label
+    {{ $attributes->filter(fn ($value, $key) => !in_array($key, ['readonly', 'disabled']))->merge(['class' => 'radio']) }}>
+    <input type="radio" name="{{ $name }}" class="form-control d-none" value="{{ $value }}"
+        {{ $is_checked ? 'checked' : '' }}
+        {{ $attributes->filter(fn ($value, $key) => in_array($key, ['readonly', 'disabled'])) }}>
 
     <div class="replacer">
         <i class="material-icons">done</i>
