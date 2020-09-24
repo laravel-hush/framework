cad602949672f0452f89472db70de0ae7dd0bba7
scary-layer <artur.osipenko@hotmail.com>
2020-09-24 20:50:32 +0300

    feat: do not delete review of already stored images when uploading new

--- resources/views/components/inputs/file-multiple.blade.php
+++ resources/views/components/inputs/file-multiple.blade.php
@@ -1,7 +1,7 @@
 @if ($attributes['preview'] ?? false)
-<div class="images-list text-center row px-2" id="{{ $getFileId() }}">
+<div class="images-list text-center row px-2">
     @foreach ($value as $image)
-    <div class="col-2 pb-3 image">
+    <div class="col-2 pb-3 image saved">
         <img src="{{ $image }}" alt="">
         <a href="{{ Constructor::link("action:remove-image|field:$name|image:$image|id:" . request()->id) }}"
             class="btn btn-danger remove-image">
@@ -10,6 +10,7 @@
     </div>
     @endforeach
 </div>
+<div class="images-list text-center" id="{{ $getFileId() }}"></div>
 @endif
 
 <div class="custom-file">
