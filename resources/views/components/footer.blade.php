0a1255733fd3a9477dacb73a461fa58865401b99
scary-layer <artur.osipenko@hotmail.com>
2020-09-27 15:23:30 +0300

    chore: links to github and docs

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,17 @@
-<footer class="with-margin row align-items-center" id="footer">
-    <span>{{ date('Y') }} © Hush</span>
+<footer class="with-margin row align-items-center justify-content-between" id="footer">
+    <span class="pl-3 text-white font-weight-bold">{{ date('Y') }} © Hush</span>
+    <div class="d-flex">
+        <a href="https://github.com/laravel-hush/framework" target="_blank"
+            class="text-white font-weight-bold pr-3 d-flex align-items-center">
+
+            <i class="fab fa-github h6 mr-2 mt-2"></i>
+            <span>GitHub</span>
+        </a>
+        <a href="https://hush.scary-layer.com/" target="_blank"
+            class="text-white font-weight-bold pr-3 d-flex align-items-center">
+
+            <i class="fas fa-file-alt h6 mr-2 mt-2"></i>
+            <span>Docs</span>
+        </a>
+    </div>
 </footer>
