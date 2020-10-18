1460a651afc7fb9f29fa0fd11159cdf3aaa4a8ed
scary-layer <artur.osipenko@hotmail.com>
2020-10-18 12:05:09 +0300

    feat: ability to define closures in block title text property

--- resources/views/constructor/block-title.blade.php
+++ resources/views/constructor/block-title.blade.php
@@ -2,7 +2,11 @@
 <div class="headline">
 
     @isset ($block['title']['text'])
-    <span class="h1">@lang ('hush::admin.' . $block['title']['text'])</span>
+        @if ($block['title']['text'] instanceof Closure)
+            <span class="h1">{{ $block['title']['text'](get_defined_vars()) }}</span>
+        @else
+            <span class="h1">@lang ('hush::admin.' . $block['title']['text'])</span>
+        @endif
     @endisset
 
     <div class="buttons">
