11ecfd9f7e428e99814bc8a8169debd09796be9e
scary-layer <artur.osipenko@hotmail.com>
2020-10-18 12:30:13 +0300

    feat: send defined vars to block buttons link constructor, search button shoul always be the last

--- resources/views/constructor/buttons.blade.php
+++ resources/views/constructor/buttons.blade.php
@@ -1,37 +1,37 @@
 @isset ($block['title']['add'])
-@if (!is_string($block['title']['add']) || auth()->user()->permitted($block['title']['add']))
-<a href="{{ Constructor::link("page:$baseUrl.edit") }}"
-    class="btn btn-light {{ Arr::get($block, 'content.modal') ? 'in-modal' : '' }}">
-    <i class="material-icons">add</i>
-    <span>@lang ('hush::admin.add')</span>
-</a>
-@endif
-@endisset
-
-@isset ($block['title']['search'])
-<a href="#" class="btn btn-light search-button">
-    <i class="material-icons">search</i>
-    <span>@lang ('hush::admin.search')</span>
-</a>
+    @if (!is_string($block['title']['add']) || auth()->user()->permitted($block['title']['add']))
+        <a href="{{ Constructor::link("page:$baseUrl.edit") }}"
+            class="btn btn-light {{ Arr::get($block, 'content.modal') ? 'in-modal' : '' }}">
+            <i class="material-icons">add</i>
+            <span>@lang ('hush::admin.add')</span>
+        </a>
+    @endif
 @endisset
 
 @isset ($block['title']['buttons'])
-@foreach ($block['title']['buttons'] as $button)
+    @foreach ($block['title']['buttons'] as $button)
 
-@if (!isset($button['permission']) || auth()->user()->permitted($button['permission']))
-@isset ($button['form'])
-<button form="{{ $button['form'] ?? '' }}" class="submit btn {{ $button['class'] ?? 'btn-light' }}">
-    <i class="material-icons">{{ $button['icon'] }}</i>
-    <span>@lang ('hush::admin.' . $button['text'])</span>
-</button>
-@else
-<a href="{{ Constructor::link($button['link'] ?? '#') }}" class="btn {{ $button['class'] ?? 'btn-light' }}"
-    @isset ($button['in-new-tab']) target="_blank" @endisset>
-    <i class="material-icons">{{ $button['icon'] }}</i>
-    <span>@lang ('hush::admin.' . $button['text'])</span>
-</a>
+        @if (!isset($button['permission']) || auth()->user()->permitted($button['permission']))
+            @isset ($button['form'])
+                <button form="{{ $button['form'] ?? '' }}" class="submit btn {{ $button['class'] ?? 'btn-light' }}">
+                    <i class="material-icons">{{ $button['icon'] }}</i>
+                    <span>@lang ('hush::admin.' . $button['text'])</span>
+                </button>
+            @else
+                <a href="{{ Constructor::link($button['link'] ?? '#', get_defined_vars()) }}" class="btn {{ $button['class'] ?? 'btn-light' }}"
+                    @isset ($button['in-new-tab']) target="_blank" @endisset>
+                    <i class="material-icons">{{ $button['icon'] }}</i>
+                    <span>@lang ('hush::admin.' . $button['text'])</span>
+                </a>
+            @endisset
+        @endif
+
+    @endforeach
 @endisset
-@endif
 
-@endforeach
+@isset ($block['title']['search'])
+    <a href="#" class="btn btn-light search-button">
+        <i class="material-icons">search</i>
+        <span>@lang ('hush::admin.search')</span>
+    </a>
 @endisset
