a122efcffa5220046b8b5ef785002cd448af42a8
scary-layer <artur.osipenko@hotmail.com>
2020-11-03 12:36:57 +0200

    feat: custom inputs support for iterable input

--- resources/views/components/iterable-item.blade.php
+++ resources/views/components/iterable-item.blade.php
@@ -15,6 +15,39 @@ $variables['model'] = $item;
         $subinput['name'] = $prefix . "[{$subinput['name']}]";
         @endphp
 
+        @if (isset($subinput['condition']) && !call_user_func($subinput['condition'], get_defined_vars()))
+            @continue
+        @endif
+
+        @if ($subinput['type'] == 'closure')
+            <div class="col {{ $subinput['width'] ?? 'col-12' }}">
+                {!! call_user_func($subinput['closure'], get_defined_vars()) !!}
+            </div>
+            @continue
+        @endif
+
+        @if ($subinput['type'] == 'html')
+            <div class="col {{ $subinput['width'] ?? 'col-12' }}">
+                {!! $subinput['html'] !!}
+            </div>
+            @continue
+        @endif
+
+        @if ($subinput['type'] == 'hidden')
+            <x-hush-input
+                type="hidden"
+                :name="$subinput['name']"
+                :value="Constructor::value(get_defined_vars(), $subinput, $subinput['default'] ?? null)"/>
+            @continue
+        @endif
+
+        @if ($subinput['type'] == 'view')
+            <div class="col {{ $subinput['width'] ?? 'col-12' }}">
+                @include ($subinput['view'])
+            </div>
+            @continue
+        @endif
+
         <div class="col {{ $subinput['width'] ?? null }}">
             {!! Input::render($subinput, $variables) !!}
         </div>
