6e93a3ea1cdc533ec5ac4c4f47dc95291ea63fd0
scary-layer <artur.osipenko@hotmail.com>
2020-11-04 11:38:07 +0200

    fix: multirow text slugification for already created models

--- resources/views/components/inputs/multilingual/text-multirow.blade.php
+++ resources/views/components/inputs/multilingual/text-multirow.blade.php
@@ -12,7 +12,8 @@
             type="text"
             name="{{ $name . '[' . $lang->code . ']' }}"
             value="{{ $value[$lang->code] ?? '' }}"
-            class="form-control {{ $getMultilingualClassAttribute() . ' ' . ($isSluggable() && $loop->first ? 'sluggable' : '') }}"
+            class="form-control {{ $getMultilingualClassAttribute() . ' '
+                . ($isSluggable() && $loop->first && (!isset($model) || !$model->id) ? 'sluggable' : '') }}"
             placeholder="{{ $getPlaceholder() }} ({{ $lang->name }})"
             data-slugify-target="{{ $isSluggable() && $loop->first ? $attributes['slugify'] : null }}"
             {{ $attributes->except('field-width', 'label', 'slugify', 'placeholder', 'multilingual', 'multirow', 'class') }}/>
