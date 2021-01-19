641407a664b45753617056d0108db346356e5dd3
scary-layer <artur.osipenko@hotmail.com>
2021-01-19 11:48:53 +0200

    fix: do not show previews of already saved images if value is null

--- resources/views/components/inputs/file-multiple.blade.php
+++ resources/views/components/inputs/file-multiple.blade.php
@@ -1,17 +1,19 @@
 @if ($attributes['preview'] ?? false)
-    <div class="images-list text-center row px-2">
-        @foreach ($value as $image)
-            <div class="{{ $attributes['preview-width'] ?? 'col-2' }} pb-3 image saved">
-                <img src="{{ $image }}" alt="">
-                @if (!isset($attributes['delete']) || $attributes['delete'])
-                    <a href="{{ Constructor::link("action:remove-image|field:$name|image:$image|id:" . request()->id) }}"
-                        class="btn btn-danger remove-image">
-                        <span class="col">@lang('hush::admin.delete')</span>
-                    </a>
-                @endif
-            </div>
-        @endforeach
-    </div>
+    @isset($value)
+        <div class="images-list text-center row px-2">
+            @foreach ($value as $image)
+                <div class="{{ $attributes['preview-width'] ?? 'col-2' }} pb-3 image saved">
+                    <img src="{{ $image }}" alt="">
+                    @if (!isset($attributes['delete']) || $attributes['delete'])
+                        <a href="{{ Constructor::link("action:remove-image|field:$name|image:$image|id:" . request()->id) }}"
+                            class="btn btn-danger remove-image">
+                            <span class="col">@lang('hush::admin.delete')</span>
+                        </a>
+                    @endif
+                </div>
+            @endforeach
+        </div>
+    @endisset
     <div class="images-list text-center row px-2" id="{{ $getFileId() }}"></div>
 @endif
 
